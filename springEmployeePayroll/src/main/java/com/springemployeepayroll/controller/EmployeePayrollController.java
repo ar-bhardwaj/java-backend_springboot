cbea3d497765061e1e24cbfe349f1a7b5b9de68e
aditi <aditiranttanbhardwaj@gmail.com>
2025-04-23 11:21:18 +0530

    [ADITI] ADD. (UC-5) Slf4j (looger) in EmployeePayRoll

--- springEmployeePayroll/src/main/java/com/springemployeepayroll/controller/EmployeePayrollController.java
+++ springEmployeePayroll/src/main/java/com/springemployeepayroll/controller/EmployeePayrollController.java
@@ -3,11 +3,13 @@ package com.springemployeepayroll.controller;
 import com.springemployeepayroll.dto.EmpDTO;
 import com.springemployeepayroll.entities.EmpEntities;
 import com.springemployeepayroll.service.EmployeePayrollService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 @RequestMapping("/payroll")
 public class EmployeePayrollController {
 
@@ -16,18 +18,21 @@ public class EmployeePayrollController {
 
     @PostMapping("/post")
     public ResponseEntity<?> post(@RequestBody EmpDTO dto){
+        log.info("Inside post method of EmployeePayrollController");
 
         return es.addEmp(dto);
     }
 
     @GetMapping("/get/{id}")
     public ResponseEntity<?> get(@PathVariable Long id){
+
         return es.viewEmp(id);
     }
 
     @PutMapping("/put/{id}")
     public ResponseEntity<?> put(@PathVariable Long id, @RequestBody EmpDTO dto){
         return es.updateEmp(id, dto);
+
     }
 
     @DeleteMapping("/delete/{id}")
--- springEmployeePayroll/src/main/java/com/springemployeepayroll/service/EmployeePayrollService.java
+++ springEmployeePayroll/src/main/java/com/springemployeepayroll/service/EmployeePayrollService.java
@@ -4,6 +4,7 @@ package com.springemployeepayroll.service;
 import com.springemployeepayroll.dto.EmpDTO;
 import com.springemployeepayroll.entities.EmpEntities;
 import com.springemployeepayroll.repository.EmployeePayrollRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,30 +14,38 @@ import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class EmployeePayrollService {
     @Autowired
     EmployeePayrollRepository er;
 
     //add
     public ResponseEntity<?> addEmp(EmpDTO dto) {
+        log.info("Adding employee payroll");
+
         EmpEntities emp = new EmpEntities();
+
         emp.setName(dto.getName());
         emp.setSalary(dto.getSalary());
         er.save(emp);
+        log.debug("employee payroll saved");
         return new ResponseEntity<String>("added payroll", HttpStatus.OK);
     }
 
     //edit
     public ResponseEntity<?> updateEmp(Long id, EmpDTO dto) {
+        log.info("Updating employee payroll");
         Optional<EmpEntities> opt = er.findById(id);
         if (opt.isPresent()) {
             EmpEntities e = opt.get();
             e.setName(dto.getName());
             e.setSalary(dto.getSalary());
             er.save(e);
+            log.debug("updated employee payroll");
             return new ResponseEntity<String>("updated successfully", HttpStatus.OK);
         }
         else{
+            log.warn("employee payroll not found");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
@@ -44,6 +53,7 @@ public class EmployeePayrollService {
 
     //view
     public ResponseEntity<?> viewEmp(Long id) {
+        log.info("Viewing employee payroll");
         Optional<EmpEntities> opt = er.findById(id);
         if (opt.isPresent()) {
             EmpEntities e = opt.get();
@@ -51,22 +61,27 @@ public class EmployeePayrollService {
             dto.setId(e.getId());
             dto.setName(e.getName());
             dto.setSalary(e.getSalary());
+
            return new ResponseEntity<>(dto, HttpStatus.OK);
         }
         else{
+            log.warn("employee payroll not found");
             return new ResponseEntity<String>("User not Found",HttpStatus.NOT_FOUND);
         }
     }
 
     //delete
     public ResponseEntity<?> deleteEmp(Long id) {
+        log.info("Deleting employee payroll");
         Optional<EmpEntities> opt = er.findById(id);
         if (opt.isPresent()) {
             EmpEntities e = opt.get();
             er.delete(e);
+            log.debug("deleted employee payroll");
             return new ResponseEntity<>("deleted successfully", HttpStatus.OK);
         }
         else{
+            log.warn("employee payroll not found");
             return new ResponseEntity<>("Not Found!",HttpStatus.NOT_FOUND);
         }
     }
