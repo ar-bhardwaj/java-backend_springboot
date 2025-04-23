ec62ee7ecc77f440ab348f22983f199928882d3b
aditi <aditiranttanbhardwaj@gmail.com>
2025-04-23 14:23:14 +0530

    [ADITI] ADD validations and exceptions in Employee PayRoll

--- springEmployeePayroll/src/main/java/com/springemployeepayroll/dto/EmpDTO.java
+++ springEmployeePayroll/src/main/java/com/springemployeepayroll/dto/EmpDTO.java
@@ -1,13 +1,19 @@
 package com.springemployeepayroll.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+
+import lombok.NonNull;
 import lombok.Setter;
+import org.hibernate.annotations.processing.Pattern;
 
 @Getter
 @Setter
 public class EmpDTO {
 
     private Long id;
+    @NotBlank(message = "First letter should be in Capital Letter")
+    // @Pattern(regex = "^[A-Z]{1}[A-Za-z]{2,}$",message="name must be valid")
     private String name;
     private double salary;
 }
